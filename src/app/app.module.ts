d0042d085f8976a0e58753e04118686acc6c22e3
Guts 6667 <chambet.dev@gmail.com>
2023-04-05 16:21:35 +0200

    Init new app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { SuccessCompComponent } from './success-comp/success-comp.component';
-import { WarningCompComponent } from './warning-comp/warning-comp.component';
-import { ServersComponent } from './servers/servers.component';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/headerComp/headerComp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    SuccessCompComponent,
-    WarningCompComponent,
-    ServersComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule, FormsModule
--- src/app/components/headerComp/headerComp.component.spec.ts
+++ src/app/components/headerComp/headerComp.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { WarningCompComponent } from './warning-comp.component';
+import { HeaderComponent } from './headerComp.component';
 
-describe('WarningCompComponent', () => {
-  let component: WarningCompComponent;
-  let fixture: ComponentFixture<WarningCompComponent>;
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ WarningCompComponent ]
+      declarations: [ HeaderComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(WarningCompComponent);
+    fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
