ced851609e13d9afe839bbd548457f196278fbbf
Guts 6667 <chambet.dev@gmail.com>
2023-04-06 15:55:03 +0200

    Custom Properties and Event Binding V.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Angular Playground';
-  name = "Rayan"
+  constructor() {}
+  serverElements = [
+    { type: 'serverElements', name: 'Testserver', content: 'Just a test' },
+  ];
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {
+    serverName: string;
+    serverContent: string;
+  }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent,
+    });
+  }
 }
--- src/app/shared/ingredient.model.ts
+++
@@ -1,5 +0,0 @@
-export class Ingredient {
-  constructor(public name: string, public amount: number) {
-    (name = this.name), (amount = this.amount);
-  }
-}
